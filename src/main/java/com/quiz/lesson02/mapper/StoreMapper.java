2db69f0e1eb723f12ebee259a1072e7d1f6a117f
sangbpark <qkrtkdqo94@gmail.com>
2024-09-27 21:46:05 +0900

    DB연동 20240927

---
+++ src/main/java/com/quiz/lesson02/mapper/StoreMapper.java
@@ -0,0 +1,13 @@
+package com.quiz.lesson02.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.quiz.lesson02.domain.Store;
+
+@Mapper
+public interface StoreMapper {
+	
+	public List<Store> selectStoreList();
+}
